302e6732c6004e442a7ddbac184406195c9bd617
Dave McKeown <dmckeown@decisionpt.com>
2014-04-10 15:02:49 -0400

    TidyTabs v1.9.2

--- TidyTabs/Properties/AssemblyInfo.cs
+++ TidyTabs/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
-[assembly: AssemblyVersion("1.9.1")]
-[assembly: AssemblyFileVersion("1.9.1")]
+[assembly: AssemblyVersion("1.9.2")]
+[assembly: AssemblyFileVersion("1.9.2")]
